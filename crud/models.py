180e1ff87f668a1e301b39aaef7ef7c393424992
PTHARRISH <harrishpt@gmail.com>
2024-01-30 23:26:46 +0530

    explanation model fields

--- crud/models.py
+++ crud/models.py
@@ -5,6 +5,8 @@ class Position(models.Model):
 
   def __str__(self):
     return self.title
+  # when you call the foreign key without __str__ it shows (position object1) after declares when ever call the class
+  # it will display representation of self.title
 
 
 class User(models.Model):
@@ -15,4 +17,5 @@ class User(models.Model):
 
   def __str__(self):
     return "%s%s"%(self.firstname,self.lastname)
+  # display the admin panel show the table name is first and last name or else it shows (user object(1)) by default 
 

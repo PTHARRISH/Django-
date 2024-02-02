b60599eafed71a37efea5e7b23e3c79a26c92d88
PTHARRISH <harrishpt@gmail.com>
2024-02-02 22:51:53 +0530

    rest

--- rest/urls.py
+++ rest/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index ),# GET - http://127.0.0.1:8000/api/ display the courses .HTTP 405 Method Not Allowed
     path('index/',views.index2 ),
+    path('hit/',views.hit ),
+    path("get/",views.get_post),
+    path("put/<id1>",views.crud)
 ]

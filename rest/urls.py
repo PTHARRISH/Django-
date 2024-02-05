ca0b064635f5220f7b44ef68abd7f7273f01419b
PTHARRISH <harrishpt@gmail.com>
2024-02-05 23:36:59 +0530

    REST URLS

--- rest/urls.py
+++ rest/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index ),# GET - http://127.0.0.1:8000/api/ display the courses .HTTP 405 Method Not Allowed
-    path('index/',views.index2 ),
+    path('index/',views.index ),# GET - http://127.0.0.1:8000/api/ display the courses .HTTP 405 Method Not Allowed
+    path('index2/',views.index2 ),
     path('hit/',views.hit ),
     path("get/",views.get_post),
-    path("put/<id1>",views.crud)
+    path("crud/<id1>",views.crud),
+    path("person/",views.person),
+    
 ]

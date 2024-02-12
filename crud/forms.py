2392ef9abf3cf32fa684c13268be9bbb9f68a11d
PTHARRISH <harrishpt@gmail.com>
2024-02-12 22:54:52 +0530

    form validation

--- crud/forms.py
+++ crud/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from .models import User
 
-class Userform(forms.ModelForm):
-    
-    class Meta:
-        model=User
-        fields='__all__'
 
-    def __init__(self,*args,**kwargs):
-        super(Userform,self).__init__(*args,**kwargs)
-        self.fields['position'].empty_label="Select"
-        self.fields['mobile'].required=False
+class Userform(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(Userform, self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = "Select"  # It will assign the value for empty label
+        self.fields['mobile'].required = False  # it wil remove the required field false
